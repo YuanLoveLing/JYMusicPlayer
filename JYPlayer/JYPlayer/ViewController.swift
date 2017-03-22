dffea099a78edacdbe06af6e31e3c67f61305f74
Jhonny Jin <736010695@qq.com>
2017-03-22 17:13:39 +0800

    11

---
+++ JYPlayer/JYPlayer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JYPlayer
+//
+//  Created by 靳志远 on 2017/3/22.
+//  Copyright © 2017年 靳志远. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
